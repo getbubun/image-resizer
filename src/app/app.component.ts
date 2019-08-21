7c9b281a25ab89f22c7ec505a1d5fdd33e95ab18
Manab Das <getbubun@gmail.com>
2019-08-21 14:31:25 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'image-resizer';
+  imgUrl:any;
+  isValidUrl: boolean ;
+
+  constructor(private data:DataService){}
+
+  ngOnInit(){}
+
+  imageValidation(){
+    console.log(this.imgUrl);
+
+    //this.isValidUrl = 
+    
+    this.data.validateUrl(this.imgUrl).subscribe(
+      data=>{
+        console.log(data)
+      },err=>{
+        console.log(err);
+        this.isValidUrl = true;
+      }
+    )
+  
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
